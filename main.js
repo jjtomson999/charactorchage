7648787997f525a3f9d21988336cdebac59291cd
kazuki uehara <85314458+jjtomson999@users.noreply.github.com>
2023-05-25 23:33:25 +0900

    commit
    疲れた

---
+++ main.js
@@ -0,0 +1,32 @@
+const title = document.querySelector('.container h1');
+
+const btnEl = document.getElementById('btn');
+
+const animeContainerEL = document.querySelector(".anime-container");
+
+const animeNameEl=document.querySelector(".anime-name");
+
+const animeImage = document.querySelector('.anime-img');
+
+let i=Math.floor(Math.random()*100);
+btnEl.addEventListener("click", async function(){
+  try{
+   btnEl.disabled=true;
+   btnEl.disabled=false;
+   animeNameEl.innerText='Let us start';
+   animeImage.src="spinner.svg";
+   animeContainerEL.style.display="block";
+   animeNameEl.innerText='getting pic...';
+   const response = await fetch("https://api.catboys.com/img")
+   const data = await response.json();
+   animeImage.url=data;
+   animeImage.src=data.url;
+   animeNameEl.innerText=data.artist;
+   
+   title.style.color=`rgb(${i},${230},${120})`;
+
+  }catch(error){
+       console.log("error occured");
+   }
+
+});
